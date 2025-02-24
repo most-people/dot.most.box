38d9edc34bbdc6c738149eb673cd5b3c1b58e26c
大海 <iwangyang@qq.com>
2025-02-24 18:29:48 +0800

    demo

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { DotServer } from './DotServer'
 import { DotClient } from './DotClient'
 import { mostWallet, encode, decode } from './MostWallet'
 
+export type { MostWallet } from './MostWallet'
+
 const Dot = {
     DotServer,
     DotClient,
